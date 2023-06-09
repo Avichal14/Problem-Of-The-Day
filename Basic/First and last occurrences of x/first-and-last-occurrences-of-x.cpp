fe47178af35ebe2b34309a1760370658affd0288
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2023-06-10 01:21:51 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/First and last occurrences of x/first-and-last-occurrences-of-x.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+int lowerbound(int arr[],int n, int x){
+        int low=0;
+        int high=n-1;
+        int ans=n;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]>=x){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+int upperbound(int arr[],int n, int x){
+        int low=0;
+        int high=n-1;
+        int ans=n;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]>x){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+vector<int> find(int arr[], int n , int x )
+{
+    // code here
+    int lb=lowerbound(arr,n,x);
+    if(lb==n || arr[lb]!=x) return {-1,-1};
+    return {lb,upperbound(arr,n,x)-1};
+    
+}
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        int arr[n],i;
+        for(i=0;i<n;i++)
+        cin>>arr[i];
+        vector<int> ans;
+        ans=find(arr,n,x);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
