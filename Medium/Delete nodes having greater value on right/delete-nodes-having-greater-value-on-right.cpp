f7803143dd87ab3aaac0fc0bff962571e6e46d86
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2023-08-29 11:16:19 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Delete nodes having greater value on right/delete-nodes-having-greater-value-on-right.cpp
@@ -0,0 +1,109 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void print(Node *root)
+{
+    Node *temp = root;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+
+
+// } Driver Code Ends
+/*
+
+The structure of linked list is the following
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution
+{
+    public:
+   Node* ans;
+    int solve(Node* head, Node* curr){
+        if(head->next==NULL){
+            return head->data;
+        }
+       int maxi =  solve(head->next,curr->next);
+        if(head->data < maxi){
+            curr->next = head->next;
+        }
+        else{
+            maxi = head->data;
+        }
+        ans = curr;
+        return maxi;
+    }
+    Node *compute(Node *head)
+    {
+        // your code goes here
+        Node* curr = new Node(-1);
+        curr->next = head;
+        solve(head,curr);
+        return ans->next;
+    }
+    
+};
+   
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int T;
+	cin>>T;
+
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+
+		for(int i=0;i<K;i++){
+		    int data;
+		    cin>>data;
+			if(head==NULL)
+			    head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+        Solution ob;
+        Node *result = ob.compute(head);
+        print(result);
+        cout<<endl;
+    }
+}
+
+// } Driver Code Ends
