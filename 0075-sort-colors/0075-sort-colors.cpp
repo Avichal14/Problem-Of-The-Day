6c356ab408484db454fca2b3b3f35f779bef06ea
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2023-09-08 22:38:21 +0530

    Time: 5 ms (10.65%), Space: 8.3 MB (48.25%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        return sort(nums.begin(),nums.end());
+    void sortColors(vector<int>& arr) {
+       int n=arr.size();
+        int zeroc=0,onec=0,twoc=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+                zeroc++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]==1){
+                onec++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]==2){
+                twoc++;
+            }
+        }
+        int index=0;
+        while(zeroc--){
+            arr[index]=0;
+            index++;
+        }
+        while(onec--){
+            arr[index]=1;
+            index++;
+        }
+        while(twoc--){
+            arr[index]=2;
+            index++;
+        }
+        
     }
 };
