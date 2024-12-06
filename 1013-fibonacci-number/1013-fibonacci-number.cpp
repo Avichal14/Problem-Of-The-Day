784b38ef76e1e5ee4751bfa9a2d61e090c4288fc
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2024-12-06 14:16:46 +0530

    Time: 14 ms (24.17%), Space: 7.7 MB (14.93%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
-    int recsol(int n){
-        if(n==1 || n==0){
+    int fib(int n) {
+        if(n==0 || n==1){
             return n;
         }
-        int ans=recsol(n-1)+ recsol(n-2);
-        return ans;
-    }
-    int fib(int n) {
-        int ans=recsol(n);
-        return ans;
+        return fib(n-1)+fib(n-2);
     }
 };
