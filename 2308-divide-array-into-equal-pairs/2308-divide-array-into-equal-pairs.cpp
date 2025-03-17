f1d574f2c0cdd7951a3b7d474951b87241cc7394
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2025-03-17 10:38:09 +0530

    Time: 6 ms (24.13%), Space: 18.3 MB (9.58%) - LeetHub

---
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_set<int> s;
+        for(int i: nums){
+            if(s.count(i)) s.erase(i);
+            else s.insert(i);
+        }
+        return s.size()==0;
+    }
+};
