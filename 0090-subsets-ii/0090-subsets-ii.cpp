3f48183dd763bf97c5b463b08b5ff043e7425e9c
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2025-07-11 17:59:16 +0530

    Time: 6 ms (12.42%), Space: 10.4 MB (85.23%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
-void getallSub(vector<int>& nums,vector<int>& ans,int i,vector<vector<int>> &allSubsets){
-    
-        if(i==nums.size()){
-            //store value
-            allSubsets.push_back(ans);
-            return;
-        }
-        //include
-        ans.push_back(nums[i]);
-        getallSub(nums,ans,i+1,allSubsets);
-
-        ans.pop_back();
-
-        int index=i+1;
-        while(index < nums.size()  && nums[index]==nums[index-1]) index++;
-        //exclude
-        getallSub(nums,ans,index,allSubsets);
-        
-    }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> allSubsets;
-        vector<int> ans;
-
-        getallSub(nums,ans,0,allSubsets);
-        return allSubsets;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res = {{}};
+        
+        for (int num : nums) {
+            int n = res.size();
+            for (int i = 0; i < n; ++i) {
+                vector<int> subset = res[i];
+                subset.push_back(num);
+                if (find(res.begin(), res.end(), subset) == res.end()) {
+                    res.push_back(subset);
+                }
+            }
+        }
+        
+        return res;
     }
 };
