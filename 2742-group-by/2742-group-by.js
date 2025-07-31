b1579f84e4a95371d45daacf3ed83df64df246a6
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2025-07-31 20:13:09 +0530

    Time: 108 ms (38.01%), Space: 74.2 MB (95.42%) - LeetHub

---
+++ 2742-group-by/2742-group-by.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    let arr=this;
+      const grouped_Arr={};
+      for(a of arr)
+      {
+          let key=fn(a);
+          if(key in grouped_Arr)
+          {
+             grouped_Arr[key].push(a);  
+          }
+          else
+          {
+             grouped_Arr[key]=[a];  
+          }
+      }
+      return grouped_Arr;
+};
+
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
