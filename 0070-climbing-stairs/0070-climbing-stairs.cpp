571e20170609278b072f78c07f428227e7a1095d
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2025-08-07 22:35:08 +0530

    Time: 0 ms (100%), Space: 8.5 MB (52.35%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        int ways = 1;
-
-        for (int i = 1; i <= n / 2; i++) {
-            double sum = 1;
-
-            for (int j = i; j < 2 * i; j++) {
-                sum *= (double)(n - j) / (j - i + 1);
-            }
-
-            ways +=sum;
+     int climbStairs(int n, unordered_map<int, int>& memo) {
+        if (n == 0 || n == 1) {
+            return 1;
         }
+        if (memo.find(n) == memo.end()) {
+            memo[n] = climbStairs(n-1, memo) + climbStairs(n-2, memo);
+        }
+        return memo[n];
+    }
 
-        return ways;
+    int climbStairs(int n) {
+        unordered_map<int, int> memo;
+        return climbStairs(n, memo);
     }
 };
