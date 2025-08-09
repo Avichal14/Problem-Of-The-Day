ec10d022978899bd45ab94013b989216477902d3
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2025-08-09 13:56:03 +0530

    Time: 3 ms (3.53%), Space: 7.6 MB (99.98%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isPowerOfTwo(int n) {
+     bool isPowerOfTwo(int n) {
         if (n == 1 || n==2)
             return true;
         for (int i = 0; i < sqrt(n)+1; i++) {
@@ -11,16 +11,3 @@ public:
         return false;
     }
 };
-
-//  bool isPowerOfTwo(int n) {
-//         if (n==1 || n==2){
-//             return true;
-//         }
-//         for (int i=2;i<sqrt(n)+1;i++){
-//             if(pow(2,i)==n){
-//                 return true;
-//             }
-//         }
-//         return false;
-
-//     }
