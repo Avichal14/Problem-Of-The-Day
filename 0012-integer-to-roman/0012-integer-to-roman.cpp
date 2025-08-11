6af7b954e5a84ecedeecbf90e3620c716a55681d
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2025-08-11 16:36:47 +0530

    Time: 4 ms (49.05%), Space: 9.2 MB (85.97%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string intToRoman(int num) {
+     string intToRoman(int num) {
         string romans[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         int val[]={1000,900,500,400,100,90,50,40,10,9,5,4,1} ;
 
