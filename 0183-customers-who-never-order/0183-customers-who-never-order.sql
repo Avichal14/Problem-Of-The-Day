3fb13b7fb254a8d1563ba098e78c4442a35af642
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2025-09-10 07:06:33 +0530

    Time: 622 ms (23.24%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,3 +1,4 @@
 # Write your MySQL query statement below
 # Write your MySQL query statement below
+# Write your MySQL query statement below
 SELECT NAME Customers FROM CUSTOMERS WHERE ID NOT IN(SELECT CUSTOMERID FROM ORDERS);
